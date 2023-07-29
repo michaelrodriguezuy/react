9f53a2c326df5143cddda9e6cc4bee162f4b6a0e
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-07-28 21:24:02 -0300

    curso react de J Duje

--- condicional_ternario.js
+++ condicional_ternario.js
@@ -1,3 +1,23 @@
-let rol_usuario= "vendedor";
-let acceso = (rol_usuario == "administrador") ? "Acceso total" : "Acceso limitado";
+
+
+
+// en JS usabamos if else
+if(rol_usuario == "administrador"){
+    acceso = "Acceso total";
+}else{
+    acceso = "Acceso limitado";
+}
 console.log(acceso); // Acceso limitado
+
+
+
+//ternario
+let rol_usuario= "vendedor";
+let acceso = (rol_usuario === "administrador") ? "Acceso total" : "Acceso limitado";
+console.log(acceso); // Acceso limitado
+
+
+
+//usando el operador AND (&&)
+let acceso2 = (rol_usuario === "administrador") && "Acceso total"; //unicamente si la primera condicion es verdadera muestro el segundo valor
+console.log(acceso2); // false

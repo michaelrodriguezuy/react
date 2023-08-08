81a348ed7874440a8b7571f71c13b819cdc2bfc1
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-08 02:27:40 -0300

    Revert "Merge commit 'a72d2043e55ed88c9b66e577cc667f3ad6fe7bf5' into ProyectoFinal"
    This reverts commit 118582bd093e6882dc3db67b0ffcbc4082670d25, reversing
    changes made to b91cfaeb799706fbad6b149b2501b287758a7ccf.

--- j_duje_youtube/video_4/curso-react-desde-cero/src/App.js
+++ j_duje_youtube/video_4/curso-react-desde-cero/src/App.js
@@ -1,23 +1,8 @@
 
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./components/pages/home/Home";
-import Formulario from "./components/pages/formulario/Formulario";
-import Login from "./components/pages/login/Login";
-
 function App() {
   return (
     <div className="App">
       
-      <BrowserRouter>
-        <Routes>  
-          <Route path="/" element={ <Login /> }/>
-          <Route path="/home" element={ <Home /> } />
-          <Route path="/formulario" element={ <Formulario /> } />
-        </Routes>
-
-      </BrowserRouter>
-
-
     </div>
   );
 }
